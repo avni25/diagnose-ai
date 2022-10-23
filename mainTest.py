46e026f49be85165acabca418423168cee4da12e
avni25 <huseyinac@gmail.com>
2022-10-23 22:04:38 +0300

    initial commit

--- mainTest.py
+++ mainTest.py
@@ -4,12 +4,12 @@ import numpy as np
 from PIL import Image
 
 model = load_model("dianseAIepochs10.h5")
-img = cv.imread("dataset/no/no0.jpg")
-
+img = cv.imread("\dataset\no\no0.jpg")
+cv.imshow(img)
 img_arr = Image.fromarray(img, "RGB")
 
 resized_img = img_arr.resize((64, 64))
-final_img = np.array()
+final_img = np.array(resized_img)
 
 
 
